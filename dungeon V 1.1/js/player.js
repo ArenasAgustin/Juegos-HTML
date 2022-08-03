4f55c5c76973aa82eca545b0afe953ea153cc568
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-08-03 11:40:45 -0300

    a

---
+++ dungeon V 1.1/js/player.js
@@ -0,0 +1,9 @@
+//OBJETO player
+const player = function () {
+  this.x = 2;
+  this.y = 2;
+
+  this.key = false;
+
+  this.draw = () => ctx.drawImage(tileMap, 0, 32, 32, 32, this.x * sideSquare, this.y * sideSquare, sideSquare, sideSquare);
+};
